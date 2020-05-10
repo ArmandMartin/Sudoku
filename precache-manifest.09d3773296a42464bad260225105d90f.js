feeeaa7aa98d21d7e8a0619e1aeaa6d1aec4bf73
ArmandMartin <armandmrtn1369@gmail.com>
2020-05-10 00:03:40 -0500

    Updates

---
+++ precache-manifest.09d3773296a42464bad260225105d90f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143e35734cdc3cfddfec5ae93d3722e7",
+    "url": "/Sudoku/index.html"
+  },
+  {
+    "revision": "c172acae82def69f737e",
+    "url": "/Sudoku/static/css/main.edd0759d.chunk.css"
+  },
+  {
+    "revision": "54251362ea259cfadfc1",
+    "url": "/Sudoku/static/js/2.7fe1bc09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sudoku/static/js/2.7fe1bc09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c172acae82def69f737e",
+    "url": "/Sudoku/static/js/main.c501f5d7.chunk.js"
+  },
+  {
+    "revision": "a3f5768ad7e0d72bdb27",
+    "url": "/Sudoku/static/js/runtime-main.f0871473.js"
+  }
+]);
